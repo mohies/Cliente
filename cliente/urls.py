e6394953cc7bc8abc34844e596bb2cd814593d1a
mohies <mohcen.benizza.bouzid.al@iespoligonosur.org>
2025-02-03 20:04:51 +0100

    avanzado

--- cliente/urls.py
+++ cliente/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('listar-equipos/', views.equipos_lista_api, name='listar_equipos'),
     path('participantes/', views.participantes_lista_api, name='participantes_lista'),
     path('juegos/', views.juegos_lista_api, name='juegos_lista'),
-    
+    path('torneo/busqueda/', views.torneo_busqueda_simple, name='torneo_busqueda_simple'),
+    path('torneo/busqueda/avanzada/', views.torneo_busqueda_avanzada, name='torneo_buscar_avanzado_sin_validaciones'),
+    path('equipo/busqueda/avanzada/', views.equipo_busqueda_avanzada, name='equipo_busqueda_avanzada'),
+    path('participante/busqueda/avanzada/', views.participante_busqueda_avanzada, name='participante_busqueda_avanzada'),
+    path('juego/busqueda/avanzada/', views.juego_busqueda_avanzada, name='juego_busqueda_avanzada'),
     ]
